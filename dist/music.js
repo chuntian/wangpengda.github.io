f11b6f762d831b63b2cf5efda50fc8b1142d8456
王鹏达 <wangpengda@wintelia.com>
2018-12-25 23:32:14 +0800

    Site updated: 2018-12-25 23:32:13

--- dist/music.js
+++ dist/music.js
@@ -8,6 +8,12 @@ const ap = new APlayer({
         artist: '刘珂矣',
         url: 'http://up.mcyt.net/?down/46644.mp3',
         cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      },
+      {
+        name: '万水千山总是情',
+        artist: '汪明荃',
+        url: 'http://www.ytmp3.cn/down/56213.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
       }
     ]
 });
