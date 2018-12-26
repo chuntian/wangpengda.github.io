e3ca16f4a6a846574b57fa8a6a69445ae95119e8
王鹏达 <wangpengda@wintelia.com>
2018-12-26 17:54:44 +0800

    Site updated: 2018-12-26 17:54:42

--- dist/music.js
+++ dist/music.js
@@ -7,13 +7,19 @@ const ap = new APlayer({
         name: '风筝误',
         artist: '刘珂矣',
         url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        cover: 'http://img.ytmp3.cn/image/55.jpg',
       },
       {
         name: '万水千山总是情',
         artist: '汪明荃',
         url: 'http://www.ytmp3.cn/down/56213.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        cover: 'http://img.ytmp3.cn/image/36.jpg',
+      },
+      {
+        name: '秋樱',
+        artist: '山口百惠',
+        url: 'http://www.ytmp3.cn/down/56259.mp3',
+        cover: 'http://img.ytmp3.cn/image/81.jpg',
       }
     ]
 });
