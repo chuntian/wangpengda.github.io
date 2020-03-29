930d91ebe2191604de0300f906cd6ae1401a69d0
王鹏达 <wangpengda@wintelia.com>
2020-03-29 18:37:41 +0800

    Site updated: 2020-03-29 18:37:38

--- dist/music.js
+++ dist/music.js
@@ -2,41 +2,40 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: true,
-    lrcType: 3,
     audio: [
-      {
-        name: '讲不出再见',
-        artist: '谭咏麟',
-        url: '../music/bye.mp3',
-        cover: 'http://p2.music.126.net/ow3GKMyAg8hdWDRmhKl4iw==/109951163066633801.jpg',
-      },
       {
         name: '年少有为',
         artist: '李荣浩',
-        url: '../music/年少有为.mp3',
+        url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_47502984&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
         cover: 'http://p2.music.126.net/tt8xwK-ASC2iqXNUXYKoDQ==/109951163606377163.jpg',
       },
       {
-        name: 'Speak Softly Love',
-        artist: 'Various Artists',
-        url: '../music/Speak Softly Love.mp3',
-        cover: 'http://p1.music.126.net/CsuwG2UBpX2-lgyDw_BMig==/4405743092508767.jpg',
-      }
-      ,
+        name: '回答',
+        artist: '张玉琪',
+        url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_73022154&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
+        cover: 'http://p2.music.126.net/A_oeydCQ7J10JF0wLQqsaA==/109951163004306956.jpg',
+      },
+      
       {
         name: '麻雀',
         artist: '李荣浩',
-        url: '../music/麻雀.mp3',
-        lrc: '../music/bird.lrc',
+        url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_83001418&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
         cover: 'http://p2.music.126.net/tt8xwK-ASC2iqXNUXYKoDQ==/109951163606377163.jpg',
       }
       ,
       {
         name: '青春',
         artist: 'Kim Feel&金昌完',
-        url: '../music/青春韩语.mp3',
-        lrc: '../music/青春韩语.lrc',
+        url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_6865922&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
         cover: 'http://p2.music.126.net/A_oeydCQ7J10JF0wLQqsaA==/109951163004306956.jpg',
       }
+      ,
+      {
+        name: 'OutSide',
+        artist: '张玉琪',
+        url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_91702965&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
+        cover: 'http://p2.music.126.net/A_oeydCQ7J10JF0wLQqsaA==/109951163004306956.jpg',
+      }
+      
     ]
 });
